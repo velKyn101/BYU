ebb97cd7a366a756cb25a80325e8986753bead72
diniz12 <kelvynjdz@gmail.com>
2024-03-07 20:25:27 -0300

    Testing

--- cse210-projects-main/prepare/Learning02/Program.cs
+++ cse210-projects-main/prepare/Learning02/Program.cs
@@ -1,9 +1,18 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+
+        Job job2 = new Job();
+        job2._jobTitle = "Software Engineer";
+        job2._company = "Apple";
+
+        job1._company.Display();
     }
 }
